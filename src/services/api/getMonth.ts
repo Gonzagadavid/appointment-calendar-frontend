f66e7dbf6ba0171ec482785084360ba16a935a29
David Gonzaga <david.blesses@hotmail.com>
2023-08-19 14:12:07 -0300

    Update getMonth URL

--- src/services/api/getMonth.ts
+++ src/services/api/getMonth.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const getMonth = async (month: string, year: number) => {
-  const url = `https://calendar-json-api.up.railway.app/month/${month}?year=${year}`;
+  const url = `https://calendar-json-app.adaptable.app/month/${month}?year=${year}`;
   const { data } = await axios.get(url);
   const board = data[month];
   const monthCurrent = board.length === 6 ? board : [...board, Array(7).fill(0)];
